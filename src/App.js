84a47051a329be355bae2b8652565947b547a819
oklex <alexkim.347@gmail.com>
2020-05-27 15:26:17 -0700

    applied react-particles-js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Particles from 'react-particles-js';
 
-const logo = 'Logo.svg'
+const logo = "Logo.svg";
+const fullLogo = "FullLogo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+      <Particles
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 160,
+	            "density": {
+	                "enable": false
+	            }
+	        },
+	        "size": {
+	            "value": 10,
+	            "random": true
+	        },
+	        "move": {
+	            "direction": "bottom",
+	            "out_mode": "out"
+	        },
+	        "line_linked": {
+	            "enable": false
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onclick": {
+	                "enable": true,
+	                "mode": "remove"
+	            }
+	        },
+	        "modes": {
+	            "remove": {
+	                "particles_nb": 10
+	            }
+	        }
+	    }
+	}} />
+				<div className='App-body'>
+					<img src={fullLogo} className="App-logo" alt="logo" />
+					<p>Coming April 2021</p>
+				</div>
+		</div>
+	);
 }
 
 export default App;
