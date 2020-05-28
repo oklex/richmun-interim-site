3a7651a65d9453177ce5fa32998b3db11b602a79
oklex <alexkim.347@gmail.com>
2020-05-27 19:55:37 -0700

    updated SEO

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,8 +4,9 @@ import ParticleBackground from "../../components/particles";
 import ReactTooltip from "react-tooltip";
 import { withRouter, Redirect } from "react-router-dom";
 import DynamicLogo from "../../components/dynamicLogo";
-// import helmet from 'react-helmet'
+import {Helmet} from "react-helmet";
 
+const upcomingYear = "2021"
 
 class Home extends React.Component {
 	state = {
@@ -37,11 +38,18 @@ class Home extends React.Component {
 	render() {
 		return (
 			<div className="App">
+				<Helmet>
+					<meta charSet="utf-8" />
+					<title>Transitioning to {upcomingYear}</title>
+					<link rel="canonical" href="interim.richmun.ca"/>
+					<meta name="robots" content="index, follow" />
+					<meta name="description" content={"Richmond Model United Nations is hiring for " + upcomingYear}></meta>
+				</Helmet>
 				{this.showTransition()}
 				<ReactTooltip place="bottom" type="dark" effect="float" />
 				<div className="App-body">
 					<div className="App-header">
-						<DynamicLogo/>
+						<DynamicLogo />
 						<p className="greyText">Coming April 2021</p>
 					</div>
 					<div className="Links container d-flex flex-wrap justify-content-around">
