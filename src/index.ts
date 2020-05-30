fc8bcb34322bd8fd444a24f7edbdce1280cec762
oklex <alexkim.347@gmail.com>
2020-05-30 14:44:27 -0700

    removed server

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from "./app";
-const dotenv = require("dotenv").config(); // required for process.env
-
-const port = process.env.PORT || 8081;
-app.listen(port, () => {
-	console.log("App is running at port " + port);
-});
